642a709ddc80d33826cf76518af788181bec77cc
Scott Sloan - Work <scott.sloan@agropur.com>
2015-05-06 00:58:26 -0500

    playing with auto resizing

--- js/SlideShow.js
+++ js/SlideShow.js
@@ -25,6 +25,12 @@
 	//private functions
 	function init(options){		
 		settings = $.extend({
+			width: 960,
+			height: 700,
+			margin: 0,
+			minScale: 0.2,
+			maxScale: 1.5, 
+			
 			displayLength : 1000,
 			containerID: '#slideDeck',
 			slideClass: '.slide',
@@ -35,6 +41,7 @@
 		dom.slides = document.querySelector(settings.slideClass);
 	
 		reload();
+		resizeSlides();
 		start();
 	};
 	
@@ -144,6 +151,149 @@
 		_slideIdx = 1;
 		_prevIdx = 0;	
 	};
+
+	/* Dom Manuipulation */
+	function resizeSlides(){
+		if(dom.slideDeck){
+			var size = ComputeSlideSize();
+			var padding = 20;
+			
+			resizeSlideContents(size.width, size.height, padding)
+			
+			for(var i = 0; i < dom.slides.length; i++){
+				var element = dom.slides[i];
+				
+				element.style.width = size.width + 'px';
+				element.style.height = size.height + 'px';
+				
+				var slideDeckSize = ComputeSlideDeckSize();
+				var scale = Math.min( slideDeckSize.width / size.width, slideDeckSize.height / size.height );
+	
+				// Respect max/min scale settings
+				scale = Math.max( scale, settings.minScale );
+				scale = Math.min( scale, settings.maxScale );
+				
+				// Don't apply any scaling styles if scale is 1
+				if( scale === 1 ) {
+					element.style.zoom = '';
+					element.style.left = '';
+					element.style.top = '';
+					element.style.bottom = '';
+					element.style.right = '';
+					transformElement(element, '' );
+				}
+				else {
+					// Prefer zooming in desktop Chrome so that content remains crisp
+					if( /chrome/i.test( navigator.userAgent ) && typeof element.style.zoom !== 'undefined' ) {
+						element.style.zoom = scale;
+					}
+					// Apply scale transform as a fallback
+					else {
+						element.style.left = '50%';
+						element.style.top = '50%';
+						element.style.bottom = 'auto';
+						element.style.right = 'auto';
+						transformElement( element, 'translate(-50%, -50%) scale('+ scale +')' );
+					}
+				}
+			}
+			
+
+		}
+	}
+	
+	function resizeSlideContents(width, height, padding){
+		for(var i = 0; i < dom.slides.length; i++){
+			var element = dom.slides[i];
+			// Determine how much vertical space we can use
+			var remainingHeight = getRemainingHeight( element, height );
+
+			// Consider the aspect ratio of media elements
+			if( /(img|video)/gi.test( element.nodeName ) ) {
+				var nw = element.naturalWidth || element.videoWidth,
+					nh = element.naturalHeight || element.videoHeight;
+
+				var es = Math.min( width / nw, remainingHeight / nh );
+
+				element.style.width = ( nw * es ) + 'px';
+				element.style.height = ( nh * es ) + 'px';
+
+			}
+			else {
+				element.style.width = width + 'px';
+				element.style.height = remainingHeight + 'px';
+			}
+		}
+	}
+	
+	function getRemainingHeight( element, height ) {
+		height = height || 0;
+
+		if( element ) {
+			var newHeight, oldHeight = element.style.height;
+
+			// Change the .stretch element height to 0 in order find the height of all
+			// the other elements
+			element.style.height = '0px';
+			newHeight = height - element.parentNode.offsetHeight;
+
+			// Restore the old height, just in case
+			element.style.height = oldHeight + 'px';
+
+			return newHeight;
+		}
+
+		return height;
+	}
+	
+	function toArray(o) {
+		return Array.prototype.slice.call(o);
+	}
+	
+	
+	function transformElement( element, transform ) {
+		if(element)
+		{
+			element.style.WebkitTransform = transform;
+			element.style.MozTransform = transform;
+			element.style.msTransform = transform;
+			element.style.OTransform = transform;
+			element.style.transform = transform;	
+		}
+	}
+		
+	function ComputeSlideSize(width, height){
+		var size = {
+			width : settings.width,
+			height : settings.height
+		};
+		
+		// Slide width may be a percentage of available width
+		if( typeof size.width === 'string' && /%$/.test( size.width ) ) {
+			size.width = parseInt( size.width, 10 ) / 100 * size.presentationWidth;
+		}
+
+		// Slide height may be a percentage of available height
+		if( typeof size.height === 'string' && /%$/.test( size.height ) ) {
+			size.height = parseInt( size.height, 10 ) / 100 * size.presentationHeight;
+		}
+		
+		return size; 		
+	}
+	
+	function ComputeSlideDeckSize(width, height)
+	{
+		var size = {
+			width : width || dom.slideDeck.offsetWidth,
+			height : height || dom.slideDeck.offsetHeight
+		};
+		
+		size.width  -= (size.height * settings.margin);
+		size.height -= (size.height * settings.margin);
+		
+		return size; 
+	}
+	
 
 	var SlideShow = {
 		initialize : init,
